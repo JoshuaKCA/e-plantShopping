3fe744523ae21b70caf1b8563d1eb683948db4ad
JoshuaKCA <arcilla.joshuakyle@ue.edu.ph>
2026-06-11 11:51:22 +0800

    react config fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping/", // Make sure this matches your GitHub repo name exactly!
   plugins: [react()],
-})
+});
